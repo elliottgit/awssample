0bea3d3fe291a54abd3b66364fab5eb41ea40373
Elliott N <elliottning@gmail.com>
2017-11-14 23:17:19 -0800

    Change to the enter style

--- mount.sh
+++ mount.sh
@@ -7,18 +7,13 @@
 
 #==============================================================================
 
-# set variables
-export ebsdevice=
-# export ebsuuid=
-
-#==============================================================================
-
-# display variables
-echo "EBS Device name is $ebsdevice."
-# echo "EBS Device uuid is $ebsuuid."
+# get device name
+fdisk -l
+echo -e "Enter the EBS device name:"
+read ebsdevice
 
 # check variables
-echo -e "Check the EBS volume variables and type y to start the script:"
+echo -e "EBS device name is $ebsdevice, type y to continue"
 read input
 if [ $input == 'y' ]
 then
@@ -30,14 +25,17 @@ then
   mount $ebsdevice /ebsvolume
   
   # edit mount fstab
+  file -s $ebsdevice
+  echo -e "Enter the EBS UUID:"
+  read ebsuuid
   echo "Mount EBS volume on every system reboot."
   sudo cp /etc/fstab /etc/fstab.orig
-  echo -e "$ebsdevice /ebsvolume ext4 defaults,nofail 0 2" >> /etc/fstab
+  echo -e "$ebsuuid  /ebsvolume  ext4  defaults,nofail  0  2" >> /etc/fstab
   echo "Mount directory is /ebsvolume."
-  
+
 else
 
-  echo "Edit the variables and try again"
+  echo "Retry the script"
 
 fi
 
