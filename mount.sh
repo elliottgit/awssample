5cb2d96c92ea970afd5ce9a8d7cc23b9dd2df480
Elliott N <elliottning@gmail.com>
2017-11-14 22:09:15 -0800

    File creation

---
+++ mount.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#title           :mount.sh
+#description     :Bash script to mount ebs block volume.
+#author		       :Elliott Ning
+#date            :20171113
+#version         :1.2
+
+#==============================================================================
+
+# set variables
+export ebsdevice=
+export ebsuuid=
+
+#==============================================================================
+
+# display variables
+echo "EBS Device name is $ebsdevice."
+echo "EBS Device uuid is $ebsuuid."
+
+# check variables
+echo -e "Check the EBS volume variables and type y to start the script:"
+read input
+if [ $input == 'y' ]
+then
+
+  # mount volume
+  echo "Mount EBS block volume."
+  mkfs -t ext4 $ebsdevice
+  echo "Mount directory is /ebsvolume."
+  mkdir /ebsvolume
+  mount $ebsdevice /ebsvolume
+  
+  # edit mount fstab
+  echo "Mount EBS volume on every system reboot."
+  sudo cp /etc/fstab /etc/fstab.orig
+  echo '$ebsuuid /ebsvolume ext4 defaults,nofail 0 2' >> /etc/fstab
+
+else
+
+  echo "Edit the variables and try again"
+
+fi
+
+#EOF
