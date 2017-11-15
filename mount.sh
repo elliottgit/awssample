324fd7a69c99ea71dba02e702ed44006738ed7ed
Elliott N <elliottning@gmail.com>
2017-11-14 23:23:08 -0800

    fine tune remove if statement

--- mount.sh
+++ mount.sh
@@ -12,31 +12,21 @@ fdisk -l
 echo -e "Enter the EBS device name:"
 read ebsdevice
 
-# check variables
-echo -e "EBS device name is $ebsdevice, type y to continue"
-read input
-if [ $input == 'y' ]
-then
-
-  # mount volume
-  echo "Mount EBS block volume."
-  mkfs -t ext4 $ebsdevice
-  mkdir /ebsvolume
-  mount $ebsdevice /ebsvolume
+# mount volume
+echo "Mount EBS block volume."
+mkfs -t ext4 $ebsdevice
+mkdir /ebsvolume
+mount $ebsdevice /ebsvolume
   
-  # edit mount fstab
-  file -s $ebsdevice
-  echo -e "Enter the EBS UUID:"
-  read ebsuuid
-  echo "Mount EBS volume on every system reboot."
-  sudo cp /etc/fstab /etc/fstab.orig
-  echo -e "$ebsuuid  /ebsvolume  ext4  defaults,nofail  0  2" >> /etc/fstab
-  echo "Mount directory is /ebsvolume."
+# get device uuid
+file -s $ebsdevice
+echo -e "Enter the EBS UUID:"
+read ebsuuid
 
-else
-
-  echo "Retry the script"
-
-fi
+# update mount fstab
+echo "Mount EBS volume on every system reboot."
+sudo cp /etc/fstab /etc/fstab.orig
+echo -e "$ebsuuid  /ebsvolume  ext4  defaults,nofail  0  2" >> /etc/fstab
+echo "Mount directory is /ebsvolume."
 
 #EOF
