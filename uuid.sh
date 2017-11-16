91026a3caeeed98715d548de699316c70a3834a7
Elliott N <elliottning@gmail.com>
2017-11-16 13:49:39 -0800

    Update uuid.sh

--- uuid.sh
+++ uuid.sh
@@ -4,6 +4,6 @@ fdisk -l
 echo -e "Enter the EBS device name:"
 read ebsdevice
 
-ebsuuid='blkid -s UUID -o value $ebsdevice'
+ebsuuid=uuid=`blkid -s UUID -o value $ebsdevice`
 file -s $ebsdevice
 echo "$ebsuuid"
